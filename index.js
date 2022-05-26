c9a2905f342c21f096ecd4b3ea1ab930210a514f
hanaljufri <60494167+hanaljufri@users.noreply.github.com>
2022-05-26 17:33:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ fetch ('https://api.data.gov.sg/v1/environment/psi')
                 .then(data =>  {
                     console.log(data);
                     let item=data.items[0].readings;
-                    console.log(item)
-                    //console.log(items.west)
+                    date = data.items[0].update_timestamp
+                    //console.log(item[i].o3_sub_index.west)
+                    no_of_rows =Object.keys(item).length
 
-                    for (var i=0; i<item.length; i++)
+                    for (var i=0; i<no_of_rows; i++)
                     {
-                        var obj = item[i]
-                        console.log(obj.west)
-                        // var table = document.getElementById("psitable");
-                        // var row = table.insertRow(i+1);
-                        // box6 = row.insertCell(0);
-                        // box5 = row.insertCell(0);
-                        // box4 = row.insertCell(0);
-                        // box3 = row.insertCell(0);
-                        // box2 = row.insertCell(0);
-                        // box1 = row.insertCell(0);
-                        // box0 = row.insertCell(0);
+                        //var obj = Object.keys(item)[i]
+                        
+                        var table = document.getElementById("psitable");
+                        var row = table.insertRow(i+1);
+                        box6 = row.insertCell(0);
+                        box5 = row.insertCell(0);
+                        box4 = row.insertCell(0);
+                        box3 = row.insertCell(0);
+                        box2 = row.insertCell(0);
+                        box1 = row.insertCell(0);
+                        box0 = row.insertCell(0);
 
-                        // box0.innerHTML="<b>"+items[i].carpark_number+"</b>"
-                        // box1.innerHTML="<b>"+items[i].update_datetime+"</b>"
+                        nameofbox = Object.keys(item)[i]
+                        box0.innerHTML="<b>"+nameofbox+"</b>"
+                        box1.innerHTML="<b>"+nameofbox.national+"</b>"
                         // box2.innerHTML="<b>"+items[i].carpark_info[0].lot_type+"</b>"
                         // box3.innerHTML="<b>"+items[i].carpark_info[0].total_lots+"</b>"
                         // box4.innerHTML="<b>"+items[i].carpark_info[0].lots_available+"</b>"
