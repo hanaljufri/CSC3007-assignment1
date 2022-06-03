9f5e8491122e6bb67206f6087e3ad750d083c9f3
hanaljufri <60494167+hanaljufri@users.noreply.github.com>
2022-06-03 15:51:45 +0800

    works
    next step, make it nice :)

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 fetch ('https://api.data.gov.sg/v1/environment/psi')
                 .then(response => response.json())
                 .then(data =>  {
-                    console.log(data);
+                    //console.log(data);
                     let item=data.items[0].readings;
                     date = data.items[0].update_timestamp
+                    date = date.split("T")
+                    time = date[1].split("+")
                     //console.log(item[i].o3_sub_index.west)
                     no_of_rows =Object.keys(item).length
+
+                    var name = document.getElementById("h1")
+                    name.innerHTML= "<h3> PSI Info taken at "+ date[0] +" on "+ time[0]+"</h3>"
 
                     for (var i=0; i<no_of_rows; i++)
                     {
@@ -22,11 +27,14 @@ fetch ('https://api.data.gov.sg/v1/environment/psi')
                         box0 = row.insertCell(0);
 
                         nameofbox = Object.keys(item)[i]
+                        console.log(item)
                         box0.innerHTML="<b>"+nameofbox+"</b>"
-                        box1.innerHTML="<b>"+nameofbox.national+"</b>"
-                        // box2.innerHTML="<b>"+items[i].carpark_info[0].lot_type+"</b>"
-                        // box3.innerHTML="<b>"+items[i].carpark_info[0].total_lots+"</b>"
-                        // box4.innerHTML="<b>"+items[i].carpark_info[0].lots_available+"</b>"
+                        box1.innerHTML="<b>"+item[nameofbox].national+"</b>"
+                        box2.innerHTML="<b>"+item[nameofbox].central+"</b>"
+                        box3.innerHTML="<b>"+item[nameofbox].west+"</b>"
+                        box4.innerHTML="<b>"+item[nameofbox].east+"</b>"
+                        box5.innerHTML="<b>"+item[nameofbox].north+"</b>"
+                        box6.innerHTML="<b>"+item[nameofbox].south+"</b>"
 
                     }
                 });
